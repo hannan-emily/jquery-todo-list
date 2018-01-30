eb3cec509432cbfe5e322890ab280466b20bf9e9
Emily Hannan <emilyhannan@Emilys-MacBook-Air.local>
2018-01-29 22:29:17 -0800

    cleaned out app.js code

--- js/app.js
+++ js/app.js
@@ -8,34 +8,6 @@ $(document).ready(function () {
     listItem.html($('#submitText').val());
     $("#toDoList").append(listItem);
     $('#submitText').val("");
-    $('#submitText').focus();
+    $('#submitText').focus;
 }))
 });
-
-
-// var listItem = function() {
-//   var newListItem = ($('#submitText').val);
-//   $('list').append(newListItem);
-//   $('#submitText').val('');
-//   // $(#submitText).focus();
-// }
-//
-// //when our submit button is clicked
-// $('button').click(function() {
-// //grab the text value from in the text section
-//   var newItem = $('#submitText').value;
-//   //grab our ul, class=demandsList, append the text as a new li
-//   $('.demandsList').append('<li>' + newItem + '</li>');
-// })
-
-
-// $('button').click(function() {
-//   console.log('you clicked the button yo');
-// });
-//
-// var newItem = $('submitText').value;
-// console.log('newItem');
-//
-// $('button').click(function () {
-//   $('.demandsList').append("<li>" + newItem + "</li>");
-// })
