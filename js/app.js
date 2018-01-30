1006eba6e9c28ad5c40d094fdca6023ef3a62d95
Emily Hannan <emilyhannan@Emilys-MacBook-Air.local>
2018-01-29 22:48:21 -0800

    new logic fo remove button

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //sing jquery, so, add this to the top to contain rest of jquery content
 $(document).ready(function () {
 
-  $('.button').on('click', (function(e) {
+  $('.button').on('click', function(e) {
     //prevent default submit until we enter in text
     e.preventDefault();
     // create new ul list
@@ -12,41 +12,21 @@ $(document).ready(function () {
     //create delete button for each listItem
     var deleteButton = $('<button>');
 
+    //work on this
+    deleteButton.attr("type", "button");
+		deleteButton.addClass('listButton').text('Delete');
+
     $("#toDoList").append(listItem);
     $(listItem).append(deleteButton);
 
     $('#submitText').val("");
     $('#submitText').focus();
-}))
-});
-
-$(document).ready(function() {
-	$('#submitButton').on('click', function(e) {
-		e.preventDefault(); //prevent the default action of submit
-		//add input to list
-		var newItem = $('<li>');
-		newItem.html($('#input').val());
-
-		var btn = $('<button>'); //create delete button
-		btn.attr("type", "button");
-		btn.addClass('listButton').text('Delete');
-
-		$('#listTable').append(newItem);
-		$(newItem).append(btn);
-
-		//remove input
-		$('#input').val(""); //usual text -- html  but this this is form input, it needs to be value
-
-		$('#input').focus();
-	})
-
-
-	// $("ul").on("click", "li", function() {
-	// 	$(this).remove();
-	// })
-
-	$('ul').on('click', 'li', function(e) {
-		$(this).remove();
-	})
-
 })
+
+//now that we've added a new list item to the ul
+//we need to be able to remove each individual li with it's paired button
+    $('ul').on('click', 'li', function(e) {
+      $(this).remove();
+    })
+
+});
