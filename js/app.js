ab963906101a74beeff77966ce779fe6675ea8f1
Emily Hannan <emilyhannan@Emilys-MacBook-Air.local>
2018-01-29 16:28:40 -0800

    added new button function for js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
-//first we need to be able to add li items to the ul
+//sing jquery, so, add this to the top to contain rest of jquery content
+$(document).ready(function () {
 
+//add in text content from input form as a new li, into parent ul list
+$('.button').click(function() {
+  $('ul').append('<li>($('input.newDemand'))</li>');
+});
+
+// $("ul").on('click', 'li', function(event) {
+//     console.log(event.target, "event delegation");
+//     //this is a child function tht works on all parent items. this changes clicked items to red
+//     $(event.target).css("color", "red");
+//   })
+// });
 //second, be able to remove li items from the ul
+
+});
